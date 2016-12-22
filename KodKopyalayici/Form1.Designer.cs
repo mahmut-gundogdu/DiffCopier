ec6b153c64b16dbaca0ed449bf1d0fb072217a6c
Mahmut Gündoğdu <mahmutgundogdu@hotmail.com.tr>
2016-12-22 14:30:24 +0300

    init

---
+++ KodKopyalayici/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace KodKopyalayici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDosyaAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbKaynakKurum = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbHedefKurum = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tortoise ile Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDosyaAdi
+            // 
+            this.txtDosyaAdi.Location = new System.Drawing.Point(110, 92);
+            this.txtDosyaAdi.Name = "txtDosyaAdi";
+            this.txtDosyaAdi.Size = new System.Drawing.Size(324, 20);
+            this.txtDosyaAdi.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dosya Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kaynak:";
+            // 
+            // cmbKaynakKurum
+            // 
+            this.cmbKaynakKurum.FormattingEnabled = true;
+            this.cmbKaynakKurum.Location = new System.Drawing.Point(110, 64);
+            this.cmbKaynakKurum.Name = "cmbKaynakKurum";
+            this.cmbKaynakKurum.Size = new System.Drawing.Size(324, 21);
+            this.cmbKaynakKurum.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(482, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Hedef Kurum:";
+            // 
+            // cmbHedefKurum
+            // 
+            this.cmbHedefKurum.FormattingEnabled = true;
+            this.cmbHedefKurum.Location = new System.Drawing.Point(560, 64);
+            this.cmbHedefKurum.Name = "cmbHedefKurum";
+            this.cmbHedefKurum.Size = new System.Drawing.Size(324, 21);
+            this.cmbHedefKurum.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(621, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Beyond Compare ile Aç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(770, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Kopyala";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 227);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.cmbHedefKurum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbKaynakKurum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDosyaAdi);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDosyaAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbKaynakKurum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbHedefKurum;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ KodKopyalayici/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KodKopyalayici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var kurumAdlari = this.KurumlariGetir();
+            var kurumAdlari2 = this.KurumlariGetir();
+            this.cmbKaynakKurum.DataSource = kurumAdlari;
+            this.cmbHedefKurum.DataSource = kurumAdlari2;//yoksa birini seçince diğeri de değişiyor.
+
+        }
+
+        private string AyarlarDosyaAdi = "Ayarlar.txt";
+        public string[] KurumlariGetir()
+        {
+            return File.ReadAllLines(AyarlarDosyaAdi);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string anaDosya, hedefDosya;
+            anaDosya = Path.Combine(cmbKaynakKurum.Text, txtDosyaAdi.Text);
+            hedefDosya = Path.Combine(cmbHedefKurum.Text, txtDosyaAdi.Text);
+
+            await TortoiseDiffAsync(anaDosya, hedefDosya);
+            SonrakiHedef();
+        }
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string anaDosya, hedefDosya;
+            anaDosya = Path.Combine(cmbKaynakKurum.Text, txtDosyaAdi.Text);
+            hedefDosya = Path.Combine(cmbHedefKurum.Text, txtDosyaAdi.Text);
+
+            await BeyondeCompareDiffAsync(anaDosya, hedefDosya);
+            SonrakiHedef();
+        }
+        public void SonrakiHedef()
+        {
+            var index = cmbHedefKurum.SelectedIndex;
+            index++;
+            var count = cmbHedefKurum.Items.Count;
+            if (index < count)
+            {
+                cmbHedefKurum.SelectedIndex = index;
+            }
+        }
+        private Task BeyondeCompareDiffAsync(string AnaDosya, string HedefDosya)
+        {
+            return Task.Run(() =>
+            {
+
+                //Open Tortoise for show diff
+                var commandLineArgument = string.Format("\"{0}\" \"{1}\" /title1=\"{0}\" /title2=\"{1}\"",
+                    AnaDosya,
+                    HedefDosya
+                    );
+
+                var exeName = @"C:\Users\Hplaptop6570b\Desktop\Beyond Compare 4\BComp.exe";
+
+                System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(exeName, commandLineArgument);
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                p.StartInfo = info;
+                p.Start();
+                p.WaitForExit(); 
+            });
+
+        }
+
+        private Task TortoiseDiffAsync(string AnaDosya, string HedefDosya)
+        {
+            return Task.Run(() =>
+            {
+
+                //Open Tortoise for show diff
+                var commandLineArgument = string.Format("/base:\"{0}\" /mine:\"{1}\" /basename:\"Öncesi\" /minename:\"Sonrası\"",
+                    AnaDosya,
+                    HedefDosya
+                    );
+
+                var exeName = @"C:\Program Files\TortoiseSVN\bin\TortoiseMerge.exe";
+
+                System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo(exeName, commandLineArgument);
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                p.StartInfo = info;
+                p.Start();
+                p.WaitForExit();
+                
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string anaDosya, hedefDosya;
+            anaDosya = Path.Combine(cmbKaynakKurum.Text, txtDosyaAdi.Text);
+            hedefDosya = Path.Combine(cmbHedefKurum.Text, txtDosyaAdi.Text);
+            if(anaDosya != hedefDosya && File.Exists(anaDosya))
+            File.Copy(anaDosya, hedefDosya, false);
+            SonrakiHedef();
+        }
+    }
+}
